9b0b27f21d194b56efb5fad07a749dd6ef5a0931
Luca Bracone <luca.bracone@elca.ch>
2023-12-01 10:09:07 +0100

    day 1 cleanup

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,23 +1,35 @@
 use std::time::Instant;
 
 fn p1(input: &str) -> u32 {
-    input.lines().map(|l| {
-        let first = l.chars().find(|c| c.is_numeric()).and_then(|c| c.to_digit(10)).unwrap();
-        let last = l.chars().rfind(|c| c.is_numeric()).and_then(|c| c.to_digit(10)).unwrap();
-        first * 10 + last
-    }).sum()
+    input
+        .lines()
+        .map(|l| {
+            let first = l
+                .chars()
+                .find(|c| c.is_numeric())
+                .and_then(|c| c.to_digit(10))
+                .unwrap();
+            let last = l
+                .chars()
+                .rfind(|c| c.is_numeric())
+                .and_then(|c| c.to_digit(10))
+                .unwrap();
+            first * 10 + last
+        })
+        .sum()
 }
 
 fn p2(input: &str) -> u32 {
-    let replaced = input.replace("one", "one1one")
-        .replace("two", "two2two")
-        .replace("three", "three3three")
-        .replace("four", "four4four")
-        .replace("five", "five5five")
-        .replace("six", "six6six")
-        .replace("seven", "seven7seven")
-        .replace("eight", "eight8eight")
-        .replace("nine", "nine9nine");
+    let replaced = input
+        .replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e");
     p1(&replaced)
 }
 
@@ -35,6 +47,7 @@ fn main() {
     println!("took: {:?}", end2.duration_since(start2));
 }
 
+#[cfg(test)]
 mod test_day1 {
     use super::*;
 
@@ -58,3 +71,4 @@ zoneight234
         assert_eq!(p2(input), 281);
     }
 }
+
